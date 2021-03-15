7a526df0a00c23740144f671b3f3e49c6703113a
chenkan <214504989@qq.com>
2021-03-15 17:50:41 +0800

    modify B differ Win32 and Linux
    Signed-off-by: chenkan <214504989@qq.com>

--- cmake_project_sdk/classB_Lib/include/classB.h
+++ cmake_project_sdk/classB_Lib/include/classB.h
@@ -2,8 +2,14 @@
 #define CLASSB
 
 #include <iostream>
+#ifdef linux
+    #define BCLASS_API 
+#elif defined(WIN32)
+    #define BCLASS_API __declspec(dllexport)
+#endif
 
-class B
+
+class BCLASS_API B
 {
 public:
     B(){}
